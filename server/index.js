6bf7c133f0384abf03035cbbd78159956b1c09e9
Todd Everett Griffin <goddtriffin@gmail.com>
2019-01-28 23:02:04 -0500

    Gitignore real .env file. Create sample .env file. Add port setting to dotenv.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send("Hello World!"));
 
