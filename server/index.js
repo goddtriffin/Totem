487c0e0f1db55b12fcbd268826404b9a677c9178
Todd Everett Griffin <goddtriffin@gmail.com>
2019-01-28 21:36:55 -0500

    Add 404 error to unused routes.

--- server/index.js
+++ server/index.js
@@ -4,4 +4,10 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log('Listening on http://localhost:' + port));
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+});
+
+const server = app.listen(port, () => {
+    console.log('Listening on http://localhost:' + port)}
+);
