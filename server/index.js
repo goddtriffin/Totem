fbb891164ac1d75eb175099d71c2e071072976da
Todd Everett Griffin <goddtriffin@gmail.com>
2019-01-29 19:02:48 -0500

    Create API endpoints for getting a list of all users and signing a user up. Password hashing is not implemented yet. Create method of validating API query parameters. Update db_init tool to bring it to spec with the new understanding of project requirements. Add NODE_ENV variable to distinguish between development and production server/database.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,65 @@
-const express = require('express');
-const app = express();
-
 require('dotenv').config();
+const app = require('express')();
 
-const port = process.env.PORT;
-
-app.get('/', (req, res) => res.send("Hello World!"));
-
-app.use((req, res, next) => {
-    res.status(404).send("404 ;)")
+const db = require('knex')({
+    client: "sqlite3",
+    connection: {
+        filename: "./db/" + ((process.env.NODE_ENV === 'production')? 'prod' : 'dev') + ".db"
+    },
+    useNullAsDefault: true,
+    debug: true,
+    asyncStackTraces: true
 });
 
-const server = app.listen(port, () => {
-    console.log('Listening on http://localhost:' + port)}
-);
+// basic url
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// get a list of all users
+app.get('/api/users', async (req, res) => {
+    const result = await db
+        .select('id', 'email', 'username', 'display_name', 'hash')
+        .table('users');
+
+    res.status(200).send(result);
+});
+
+// sign a new user up
+app.get('/api/signup', async (req, res) => {
+    const requirements = {
+        email: req.query.email,
+        username: req.query.username,
+        display_name: req.query.display_name,
+        hash: req.query.password
+    };
+
+    if (!validateObject(requirements, ['email', 'username', 'display_name', 'hash'])) {
+        res.status(400).send('400: malformed parameters; /api/?email=&username=&display_name=&password=');
+        return;
+    }
+
+    const result = await db
+        .insert(requirements)
+        .into('users')
+        .returning('id')
+        .catch(e => {
+            res.status(400).send(e);
+            return;
+        });
+    
+    res.status(200).send(result);
+});
+
+// any other url, send 404
+app.use((req, res, next) => {
+    res.status(404).send('404 ;)');
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on http://localhost:' + server.address().port);
+});
+
+function validateObject(object, properties) {
+    return properties.every(p => object[p]);
+}
--- server/tools/db_init.js
+++ server/tools/db_init.js
@@ -22,13 +22,66 @@ async function create_users_table() {
     await db.schema.hasTable('users').then(exists => {
         if (!exists) {
             return db.schema.createTable('users', table => {
-                table.increments('id');
-	
                 table.string('email').unique();
                 table.string('username').unique();
                 table.string('display_name');
+                table.string('hash');
 
-                table.string("hash");
+                table.string('emoji');  // make this a char?
+                
+                table.integer('friend_challenges');
+                table.integer('friend_challenges_won');
+                table.integer('tiki_score');  // 2x tikis on votes to your picture, 1x tikis on votes to the opponents pictures
+                table.integer('polls_created');
+            });
+        }
+    });
+}
+
+async function create_friends_table() {
+    await db.schema.hasTable('friends').then(exists => {
+        if (!exists) {
+            return db.schema.createTable('friends', table => {
+                table.string('username_1');
+                table.string('username_2');
+                table.string('state');  // pending , accepted
+            });
+        }
+    });
+}
+
+async function create_polls_table() {
+    await db.schema.hasTable('polls').then(exists => {
+        if (!exists) {
+            return db.schema.createTable('polls', table => {
+                table.increments('id').unique();
+                table.string('display_name');
+                table.string('theme');
+
+                table.string('creator');  // user table 'username'
+                table.string('opponent').nullable();  // user table 'username'
+                table.string('image_1');  // filepath
+                table.string('image_2').nullable();;  // filepath
+                table.integer('votes_1');
+                table.integer('votes_2');
+
+                table.string('state');  // pending, ready, active, expired
+                table.string('type');  // private, public
+                table.integer('duration');
+                table.timestamp('start_time').nullable();  // created when state changes from 'ready' to 'active'
+                table.timestamp('end_time').nullable();  // created when state changes from 'ready' to 'active'
+            });
+        }
+    });
+}
+
+async function create_history_table() {
+    await db.schema.hasTable('history').then(exists => {
+        if (!exists) {
+            return db.schema.createTable('history', table => {
+                table.string('username');
+                table.integer('post');
+                table.integer('vote');  // 1=creator , 2=opponent
             });
         }
     });
@@ -36,6 +89,8 @@ async function create_users_table() {
 
 (async () => {
     await create_users_table();
+    await create_friends_table();
+    await create_polls_table();
 })().then(() => {
     process.exit(0);
 }).catch(e => {
