baa409fd53d217390942b30eb35d318c927a3a7a
niralirai <niralirai@yahoo.com>
2019-02-07 16:48:24 -0500

    pulled signup page inputs in to js file and printed

---
+++ webapp/Signup/signup.js
@@ -0,0 +1,8 @@
+function display() {
+   console.log( document.forms["signupform"]["username"].value);  
+   console.log( document.forms["signupform"]["displayName"].value);  
+   console.log( document.forms["signupform"]["email"].value);  
+   console.log( document.forms["signupform"]["password"].value);  
+   console.log( document.forms["signupform"]["passwordVerify"].value);  
+
+}
