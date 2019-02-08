0de4556a4f887e4e7687405db6f77a31fb5724e6
niralirai <niralirai@yahoo.com>
2019-02-08 08:25:35 -0500

    login page method set up for moving to next page given no errors

--- webapp/Login/login.js
+++ webapp/Login/login.js
@@ -3,16 +3,22 @@
 function login(){
 	var username = loginform.username.value;
 	var password = loginform.password.value;
-	verifyInput(username, password);
+
+	if(verifyInput(username, password)){
+		// move to next page
+		console.log("move to next page");
+	}
+
 }
 
 
 function verifyInput(username, password){	
+	var has_error = false;
 	//username
 	if(!username){
 		document.getElementById("usernameError").innerHTML = "Please enter a Username";
+		has_error = true;
 	}
-	
 	else{
 		document.getElementById("usernameError").innerHTML = "";
 	}
@@ -20,9 +26,15 @@ function verifyInput(username, password){
 	//passwords
 	if(!password){
 		document.getElementById("passwordError").innerHTML = "Please enter a password";
+		has_error = true;
 	}
 	else{
 		document.getElementById("passwordError").innerHTML = "";
 	}
+
+	if(has_error){
+		return 0;
+	}
+	return 1;
 }
 
