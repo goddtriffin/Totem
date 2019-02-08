e163543155bdb1a9f6a702493d12ccdab966672b
niralirai <niralirai@yahoo.com>
2019-02-07 19:50:41 -0500

    changed pull inputs method - cleanup

--- webapp/Login/login.js
+++ webapp/Login/login.js
@@ -1,6 +1,6 @@
 
 
 function login(){
-	var username = document.forms["loginform"]["username"].value;
-	var password = document.forms["loginform"]["password"].value;
+	var username = loginform.username.value;
+	var password = loginform.password.value;
 }
--- webapp/Signup/signup.js
+++ webapp/Signup/signup.js
@@ -1,10 +1,10 @@
 
 
 function CreateUser() {
-	var username = document.forms["signupform"]["username"].value;
-	var displayName =  document.forms["signupform"]["displayName"].value;
-	var email = document.forms["signupform"]["email"].value;
-	var password = document.forms["signupform"]["password"].value;
-	var passwordVerify = document.forms["signupform"]["passwordVerify"].value;
+	var username = signupform.username.value;
+	var displayName =  signupform.displayName.value;
+	var email = signupform.email.value;
+	var password = signupform.password.value;
+	var passwordVerify = signupform.passwordVerify.value;
 
 }
