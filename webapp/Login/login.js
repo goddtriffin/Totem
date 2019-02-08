5804ee443695b191c685bb6f37043047ec309c47
niralirai <niralirai@yahoo.com>
2019-02-08 00:18:07 -0500

    login frontend error checks

--- webapp/Login/login.js
+++ webapp/Login/login.js
@@ -3,4 +3,26 @@
 function login(){
 	var username = loginform.username.value;
 	var password = loginform.password.value;
+	verifyInput(username, password);
 }
+
+
+function verifyInput(username, password){	
+	//username
+	if(!username){
+		document.getElementById("usernameError").innerHTML = "Please enter a Username";
+	}
+	
+	else{
+		document.getElementById("usernameError").innerHTML = "";
+	}
+
+	//passwords
+	if(!password){
+		document.getElementById("passwordError").innerHTML = "Please enter a password";
+	}
+	else{
+		document.getElementById("passwordError").innerHTML = "";
+	}
+}
+
