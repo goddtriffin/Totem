6ec63287fc7afe0f0fb446c57466b7a3de03c39e
niralirai <niralirai@yahoo.com>
2019-02-07 19:32:54 -0500

    signup page --> user inputs stared in method in js

--- webapp/Signup/signup.js
+++ webapp/Signup/signup.js
@@ -1,8 +1,10 @@
-function display() {
-   console.log( document.forms["signupform"]["username"].value);  
-   console.log( document.forms["signupform"]["displayName"].value);  
-   console.log( document.forms["signupform"]["email"].value);  
-   console.log( document.forms["signupform"]["password"].value);  
-   console.log( document.forms["signupform"]["passwordVerify"].value);  
+
+
+function CreateUser() {
+	var username = document.forms["signupform"]["username"].value;
+	var displayName =  document.forms["signupform"]["displayName"].value;
+	var email = document.forms["signupform"]["email"].value;
+	var password = document.forms["signupform"]["password"].value;
+	var passwordVerify = document.forms["signupform"]["passwordVerify"].value;
 
 }
