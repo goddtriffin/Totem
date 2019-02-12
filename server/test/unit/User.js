fbe63d885f5f99041918abc84b28a6dbb55e4d97
Todd Everett Griffin <goddtriffin@gmail.com>
2019-02-11 20:57:03 -0500

    Add unit tests for User model .login(db, username, password). Add unit tests for User model .getByUsername(db, username).

--- server/test/unit/User.js
+++ server/test/unit/User.js
@@ -12,18 +12,68 @@ describe('User', () => {
 		});
 
 		afterEach(async () => {
-			await db.destroy()
+			await db.destroy();
+			db = null;
 		});
 
-		it('successful signup', async () => {
+		it('success', async () => {
 			const result = await User.signup(db, 'griff170@purdue.edu', 'todd', 'goddtriffin', '123', 'eggplant', 0, 0, 0, 0);
 			assert.equal(result.code, 200);
 		});
 
-		it('username already exists in users table', async () => {
+		it('username already exists', async () => {
 			await User.signup(db, 'griff170@purdue.edu', 'todd', 'goddtriffin', '123', 'eggplant', 0, 0, 0, 0);
 			const result = await User.signup(db, 'griff170@purdue.edu', 'todd', 'goddtriffin', '123', 'eggplant', 0, 0, 0, 0);
 			assert.equal(result.code, 409);
+		});
+	});
+
+	describe('login', () => {
+		before(async () => {
+			db = await db_tool.create(':memory:', true, false, true);
+			await User.signup(db, 'griff170@purdue.edu', 'todd', 'goddtriffin', '123', 'eggplant', 0, 0, 0, 0);
+		});
+
+		after(async () => {
+			await db.destroy();
+			db = null;
+		});
+
+		it('success', async () => {
+			const result = await User.login(db, 'todd', '123');
+			assert.equal(result.code, 200);
+		});
+
+		it('no account found with username', async () => {
+			const result = await User.login(db, 'nonexistent', '123');
+			assert.equal(result.code, 400);
+		});
+
+		it('wrong password', async () => {
+			const result = await User.login(db, 'todd', '1234');
+			assert.equal(result.code, 401);
+		});
+	});
+
+	describe('getByUsername', () => {
+		before(async () => {
+			db = await db_tool.create(':memory:', true, false, true);
+			await User.signup(db, 'griff170@purdue.edu', 'todd', 'goddtriffin', '123', 'eggplant', 0, 0, 0, 0);
+		});
+
+		after(async () => {
+			await db.destroy();
+			db = null;
+		});
+
+		it('success', async () => {
+			const result = await User.getByUsername(db, 'todd');
+			assert.equal(result.code, 200);
+		});
+
+		it('no account found with username', async () => {
+			const result = await User.getByUsername(db, 'nonexistent');
+			assert.equal(result.code, 400);
 		});
 	});
 });
