8aefd8dc3df7b0160bcf07974326f9d7ce726000
niralirai <niralirai@yahoo.com>
2019-02-14 11:39:05 -0500

    signup and create user

--- webapp/signup/signup.js
+++ webapp/signup/signup.js
@@ -4,8 +4,11 @@ signup = (username, displayName, email, password, passwordVerify) => {
 	
 	if(verifyInput(username, displayName, email, passwordVerify, password)){
 		// move to next page
-		// Createuser(username, displayName, email, password, passwordVerify);
 		console.log("move to next page");
+
+		Createuser(username, displayName, email, password, passwordVerify);
+		
+
 
 		return 1;
 	}
@@ -14,7 +17,43 @@ signup = (username, displayName, email, password, passwordVerify) => {
 
 function Createuser(username, displayName, email, password, passwordVerify){
 
+
+	var url = "http://localhost:3000/api/user/signup";
+
+	var data = {};
+	data.email= email;
+	data.username = username;
+	data.display_name = displayName;
+	data.password = password;
+	data.emoji = "apple";
+
+	var json = JSON.stringify(data);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+	xhr.onload = function () {
+		var users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "201") {
+			console.table(users);
+			document.getElementById("GeneralError").innerHTML = "";
+			window.location.href = '../login';
+
+		} 
+		else if(xhr.status == "409"){
+			document.getElementById("GeneralError").innerHTML = "Please use DIFF info!";
+
+		}
+
+			else {
+			console.error(users);
+		}
+	}
+	
+	xhr.send(json);
+
 }
+
 
 
 function verifyInput(username, displayName, email, passwordVerify, password){
@@ -22,12 +61,8 @@ function verifyInput(username, displayName, email, passwordVerify, password){
 	var has_error = false;
 
 	//username
-	if(!username){
+	if(username== ""){
 		document.getElementById("usernameError").innerHTML = "Please enter a Username";
-		has_error = true;
-	}
-	else if(username.length < 6){
-		document.getElementById("usernameError").innerHTML = "Please enter a valid Username";
 		has_error = true;
 	}
 	else{
@@ -35,12 +70,8 @@ function verifyInput(username, displayName, email, passwordVerify, password){
 	}
 	
 	//display name
-	if(!displayName){
+	if(displayName== ""){
 		document.getElementById("displayNameError").innerHTML = "Please enter a Display Name";
-		has_error = true;
-	}
-	else if(displayName.length < 5){
-		document.getElementById("displayNameError").innerHTML = "Please enter a valid Display Name";
 		has_error = true;
 	}
 	else{
@@ -48,32 +79,23 @@ function verifyInput(username, displayName, email, passwordVerify, password){
 	}
 
 	// email
-	var regexpEmail = new RegExp('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+');
-	if(!email){
+	if(email== ""){
 		document.getElementById("emailError").innerHTML = "Please enter your Email";
 		has_error = true;	
-	}
-	else if(!email.match(regexpEmail)){
-		document.getElementById("emailError").innerHTML = "Please enter a valid Email";
-		has_error = true;
 	}
 	else{
 		document.getElementById("emailError").innerHTML = "";
 	}
 
 	//passwords
-	if(!password){
+	if(password == ""){
 		document.getElementById("passwordError").innerHTML = "Please enter a password";
-		has_error = true;
-	}
-	else if(password.length < 8){
-		document.getElementById("passwordError").innerHTML = "Please enter a valid password";
 		has_error = true;
 	}
 	else{
 		document.getElementById("passwordError").innerHTML = "";
 	}
-	if(!passwordVerify){
+	if(passwordVerify== ""){
 		document.getElementById("passwordVerifyError").innerHTML = "Please verify your password";
 		has_error = true;
 	}
