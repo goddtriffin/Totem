cd7826fef0994dbbc2b7a82a21dd8eca56704f5c
Cameron Glass <glass10@purdue.edu>
2019-02-15 15:59:30 -0500

    Emoji and profile issue fixed

--- webapp/profile/profile.js
+++ webapp/profile/profile.js
@@ -25,14 +25,13 @@ xhr.onload = function () {
 		// fix the positioning of the emoji at the top of the pagew
 		document.getElementById("emoji").innerHTML = users.data.emoji;
 		
-		//TODO: @CameronGlass 
 		//Move data to HTML --> you need to create the place and give it the ids listed below. These will 
 		//return the numbers automatically when the page is loaded. 
-		/*
-		document.getElementById("WinRate").innerHTML = users.data.friend_challenges_won/users.data.friend_challenges;
-		document.getElementById("TikiTally").innerHTML = users.data.tiki_tally;
-		document.getElementById("PollsCreated").innerHTML = users.data.polls_created;
-		*/
+		
+		document.getElementById("WinRate").innerHTML = `Win Rate<br>`+ users.data.friend_challenges_won/users.data.friend_challenges;
+		document.getElementById("TikiTally").innerHTML = `Tiki Tally<br>`+ users.data.tiki_tally;
+		document.getElementById("PollsCreated").innerHTML = `Polls Created<br>`+ users.data.polls_created;
+		
 
 
 
