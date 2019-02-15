e9f0c5773dbcf514837a5c906b488aa2576e304a
Cameron Glass <glass10@purdue.edu>
2019-02-15 17:29:51 -0500

    Public functions, all but search working

--- webapp/public/public.js
+++ webapp/public/public.js
@@ -1,15 +1,24 @@
+//GLOBALS
+let sorting = "Newest";
 
+function createPoll(){
 
-createPoll = (title, theme, experiation, imageOne, imageTwo, privacy1, privacy2) =>{
+	let personal = document.getElementById("personalButton").classList.contains("active");
+	let challenge = document.getElementById("challengeButton").classList.contains("active");
 
-	if(privacy1 == "on"){
+	let title = document.getElementById("titleInput").value;
+	let theme = document.getElementById("themeInput");
+	theme = theme.options[theme.selectedIndex].innerHTML;
+	let duration = document.getElementById("durationDay").value + "-" + document.getElementById("durationHour").value + "-" +  document.getElementById("durationMin").value;
+	let imageOne = document.getElementById("imageOne").value;
 
-		console.log("private");
+	if(personal){
+		let imageTwo = document.getElementById("imageTwo").value;
 	}
-	else{
-		console.log("public");
+	let private = document.getElementById("privateButton").classList.contains("active");
+	let public = document.getElementById("publicButton").classList.contains("active");
 
-	}
+	console.log(personal + "\n" + challenge + "\n" + title + "\n" + theme + "\n" + duration + "\n" + imageOne + "\n" + imageTwo + "\n" + private + "\n" + public)
 }
 
 
@@ -23,17 +32,47 @@ function verifyCreatepollInput(title, theme, experiation, imageOne, imageTwo, pr
 	return 1;
 }
 
-function switchPollType(){
-	var ele = document.querySelectorAll(".btn-group > button.btn");
-	for(var i=0; i<ele.length; i++){
-		if(ele[i].checked){
-			console.log(ele[i].value);
-		}
+function switchPollType(input){
+	if(input === "Personal"){
+		document.getElementById("imageTwo").classList.remove("invisible");
+		console.log("Personal");
 	}
-	if(document.getElementById("personalButton").checked === true){
-		console.log("Personal Selected");
-	}	
 	else{
-		console.log("Challenge Selected");
+		console.log("Challenge")
+		document.getElementById("imageTwo").classList.add("invisible");
 	}
+}
+
+function movePoll(direction){
+	if(direction < 0){
+		console.log("Move Left");
+	}
+	else{
+		console.log("Move Right");
+	}
+}
+
+function updateSort(sort){
+	sorting = sort;
+	console.log(sorting);
+}
+
+function changeSort(){
+	console.log("Sorting submitted: " + sorting);
+}
+
+function search(){
+	let themes = document.getElementById("themePicker");
+	themes.getElementsByClassName('btn dropdown-toggle btn-light')[0].getAttribute("title");
+	console.log("Search criteria: " + themes);
+}
+
+function vote(side){
+	if(side == "left"){
+		console.log("Voted Left");
+	}
+	else{
+		console.log("Voted Right");
+	}
+	document.getElementById("cardContentOverlay").classList.remove("invisible");
 }
