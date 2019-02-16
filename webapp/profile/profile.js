86cd33647e41b112e01f4c7512f61a0faab51256
Cameron Glass <glass10@purdue.edu>
2019-02-16 13:19:27 -0500

    Emoji selection working on Profile

--- webapp/profile/profile.js
+++ webapp/profile/profile.js
@@ -1,52 +1,48 @@
+let emojis = ["😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛"];
+
+
 
 window.onload = function() {
 	console.log("onload worked");
   fillUserInfo();
+
+  loadEmojis();
 
 };
 
 
 function fillUserInfo(){
 	var url  = "http://localhost:3000/api/user/me";
-var xhr  = new XMLHttpRequest()
+	var xhr  = new XMLHttpRequest()
 
-xhr.open('GET', url, true)
-xhr.setRequestHeader('Authorization', 'Bearer '+localStorage.token);
+	xhr.open('GET', url, true)
+	xhr.setRequestHeader('Authorization', 'Bearer '+localStorage.token);
 
-xhr.onload = function () {
-	console.log(xhr.responseText);
-	var users = JSON.parse(xhr.responseText);
-	if (xhr.readyState == 4 && xhr.status == "200") {
-		console.log(users);
-		document.getElementById("displayName").innerHTML = users.data.display_name;
-		document.getElementById("username").innerHTML = users.data.username;
-		
-		//TODO: @CameronGlass 
-		// fix the positioning of the emoji at the top of the pagew
-		document.getElementById("emoji").innerHTML = users.data.emoji;
-		
-		//Move data to HTML --> you need to create the place and give it the ids listed below. These will 
-		//return the numbers automatically when the page is loaded. 
-		
-		document.getElementById("WinRate").innerHTML = `Win Rate<br>`+ users.data.friend_challenges_won/users.data.friend_challenges;
-		document.getElementById("TikiTally").innerHTML = `Tiki Tally<br>`+ users.data.tiki_tally;
-		document.getElementById("PollsCreated").innerHTML = `Polls Created<br>`+ users.data.polls_created;
-		
+	xhr.onload = function () {
+		console.log(xhr.responseText);
+		var users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "200") {
+			console.log(users);
+			document.getElementById("displayName").innerHTML = users.data.display_name;
+			document.getElementById("username").innerHTML = users.data.username;
+			
+			//TODO: @CameronGlass 
+			// fix the positioning of the emoji at the top of the pagew
+			document.getElementById("emoji").innerHTML = users.data.emoji;
+			
+			//Move data to HTML --> you need to create the place and give it the ids listed below. These will 
+			//return the numbers automatically when the page is loaded. 
+			
+			document.getElementById("WinRate").innerHTML = `Win Rate<br>`+ users.data.friend_challenges_won/users.data.friend_challenges;
+			document.getElementById("TikiTally").innerHTML = `Tiki Tally<br>`+ users.data.tiki_tally;
+			document.getElementById("PollsCreated").innerHTML = `Polls Created<br>`+ users.data.polls_created;
 
-
-
-	} else {
-		console.error(users);
-	}
+		} else {
+			console.error(users);
+		}
+	}	
+	xhr.send(null);
 }
-xhr.send(null);
-
-
-}
-
-
-
-
 
 
 function logout(){
@@ -54,4 +50,28 @@ function logout(){
 	localStorage.removeItem(localStorage.token);
 	window.location.href = "../splash";
 
+}
+
+function loadEmojis(){
+	let table = document.getElementById("emojiTable");
+	let tableContents = "";
+	for(let i = 0; i < emojis.length; i += 5){
+		let tr = `<tr>
+			<td class="emoji" onclick="changeEmoji(${i})" data-dismiss="modal">${emojis[i]}</td>
+			<td class="emoji" onclick="changeEmoji(${i+1})" data-dismiss="modal">${emojis[i+1]}</td>
+			<td class="emoji" onclick="changeEmoji(${i+2})" data-dismiss="modal">${emojis[i+2]}</td>
+			<td class="emoji" onclick="changeEmoji(${i+3})" data-dismiss="modal">${emojis[i+3]}</td>
+			<td class="emoji" onclick="changeEmoji(${i+4})" data-dismiss="modal">${emojis[i+4]}</td>
+		</tr>`;
+
+		tableContents += tr;
+	}
+	table.getElementsByTagName("tbody")[0].innerHTML = tableContents;
+
+}
+
+function changeEmoji(index){
+	console.log("Change Emoji: " + index);
+	console.log("New Emoji: " + emojis[index]);
+	document.getElementById("emoji").innerHTML = emojis[index];
 }
