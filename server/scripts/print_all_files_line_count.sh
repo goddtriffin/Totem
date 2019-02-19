4134edeefa6fbd5a81db5d273476a5a61f1703d9
Todd Everett Griffin <goddtriffin@gmail.com>
2019-02-19 16:31:02 -0500

    Update line-count script to print out the per-file and total non-newline line count.

--- server/scripts/print_all_files_line_count.sh
+++ server/scripts/print_all_files_line_count.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# find all files that were actually created by the developer
+echo "Including blank lines:"
+echo "----------------------"
+
+# find all files that were actually created by me
 files=$(find . -type f ! -name "*-lock*" -not -path "*db/*" -not -path "*node_modules/*")
 
 # print the line count of all files found
 wc -l $files
+
+echo
+echo "Discluding blank lines:"
+echo "----------------------"
+
+# count up all the non-newline lines on a per-file basis
+for file in $files
+do
+    lines=$(grep -v '^\s*$' $file | wc -l)
+    echo "$lines $file"
+done
+
+# now print total non-newline line count
+grep -v '^\s*$' $files | wc -l
