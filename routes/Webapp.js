a94af9a5a4851daa6e8ef683ab7d49e9bd90c674
Todd Everett Griffin <goddtriffin@gmail.com>
2019-03-02 12:47:44 -0500

    Remove unused webapp auth code.

--- routes/Webapp.js
+++ routes/Webapp.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const Auth = require('./Auth');
 
 // home route should always link to splash/
 router.get('/', (req, res) => {
     res.redirect('/splash');
 });
-
-// these routes need auth to work
-const authed_pages = ['/friends', '/personal', '/private', '/public', '/settings', '/tournament'];
-router.use(authed_pages, (req, res, next) => {
-    // if (!auth.validate(req, res)) {
-    //     if (res.headersSent) {
-    //         return;
-    //     }
-
-    //     res.redirect('/splash');
-    //     return;
-    // }
-
-    next();
-}); 
 
 // all frontend pages
 router.use('/', express.static('webapp'));
