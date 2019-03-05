ecff7cb48e535ab315be760c73f10babf2edd6cd
Todd Everett Griffin <goddtriffin@gmail.com>
2019-03-05 01:10:37 -0500

    Remove lingering console.log() from tools/utils.js.

---
+++ tools/utils.js
@@ -0,0 +1,36 @@
+function getDatetimeString() {
+    const date = new Date();
+
+    const year = date.getFullYear();
+
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? '0' : '') + month;
+
+    let day  = date.getDate();
+    day = (day < 10 ? '0' : '') + day;
+
+    let hour = date.getHours();
+    hour = (hour < 10 ? '0' : '') + hour;
+
+    let min  = date.getMinutes();
+    min = (min < 10 ? '0' : '') + min;
+
+    let sec  = date.getSeconds();
+    sec = (sec < 10 ? '0' : '') + sec;
+
+    return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+}
+
+function addMinutesToDatetime(datetime, minutes) {
+    console.log(datetime);
+    console.log(minutes);
+
+    const split = datetime.split(':');
+    split[1] = (parseInt(split[1]) + minutes).toString();
+    return split.join(':');
+}
+
+module.exports = {
+    getDatetimeString,
+    addMinutesToDatetime
+}
