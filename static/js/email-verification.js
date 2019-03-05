aca2e044572b192cf77bfa12b64377fe1898267a
Cameron Glass <glass10@purdue.edu>
2019-03-04 22:57:55 -0500

    logout added to email verify

--- static/js/email-verification.js
+++ static/js/email-verification.js
@@ -3,3 +3,16 @@ window.onload = function() {
   		window.location.replace("/");
 	}
 };
+
+function logout(){
+	// console.log("logout");
+	localStorage.removeItem("token");
+	localStorage.removeItem("displayName");
+	localStorage.removeItem("username");
+	localStorage.removeItem("emoji");
+	localStorage.removeItem("WinRate");
+	localStorage.removeItem("TikiTally");
+	localStorage.removeItem("PollsCreated");
+	window.location.href = "/";
+
+}
