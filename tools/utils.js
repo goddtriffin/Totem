f76884156b5616e2e122888504e45ca217ba9791
Todd Everett Griffin <goddtriffin@gmail.com>
2019-03-07 11:39:27 -0500

    Refactor tools/utils.js to make use of the moment.js dependency instead of a homebrew method that may or may not work.

--- tools/utils.js
+++ tools/utils.js
@@ -1,30 +1,13 @@
+const moment = require('moment');
+
+const format = 'YYYY-MM-DD HH:mm:ss';
+
 function getDatetimeString() {
-    const date = new Date();
-
-    const year = date.getFullYear();
-
-    let month = date.getMonth() + 1;
-    month = (month < 10 ? '0' : '') + month;
-
-    let day  = date.getDate();
-    day = (day < 10 ? '0' : '') + day;
-
-    let hour = date.getHours();
-    hour = (hour < 10 ? '0' : '') + hour;
-
-    let min  = date.getMinutes();
-    min = (min < 10 ? '0' : '') + min;
-
-    let sec  = date.getSeconds();
-    sec = (sec < 10 ? '0' : '') + sec;
-
-    return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+    return moment().format(format);
 }
 
 function addMinutesToDatetime(datetime, minutes) {
-    const split = datetime.split(':');
-    split[1] = (parseInt(split[1]) + minutes).toString();
-    return split.join(':');
+    return moment(datetime).add(minutes, 'm').format(format);
 }
 
 module.exports = {
